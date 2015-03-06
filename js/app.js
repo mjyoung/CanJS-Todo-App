714a01582b8c851eaad955453e1e7aadae1e861c
Michael Young <michaely@icracked.com>
2015-03-05 21:54:41 -0600

    Completed up to 45:01 of the CanJS 2.0 TodoMVC tutorial Youtube: https://www.youtube.com/watch\?v\=E9kEM9P0Lp8

--- js/app.js
+++ js/app.js
@@ -1,22 +1,33 @@
 
-can.fixture("GET /services/todos", function() {
-	var todos = [
-		{name: 'Walk the dog', completed: true, id: 0},
-		{name: 'Mow the lawn', completed: false, id: 1},
-		{name: 'Learn CanJS', completed: false, id: 2},
-		{name: 'Take a shower', completed: true, id: 3}
-	];
-	return todos;
+can.fixture({
+	"GET /services/todos": function() {
+		var todos = [
+			{name: 'Walk the dog', completed: true, id: 0},
+			{name: 'Mow the lawn', completed: false, id: 1},
+			{name: 'Learn CanJS', completed: false, id: 2},
+			{name: 'Take a shower', completed: true, id: 3}
+		];
+		return todos;
+	},
+	"DELETE /services/todos/{id}": function() {
+		return {};
+	},
+	"POST /services/todos": function() {
+		console.log("Todo created!");
+		return {id: Math.random()};
+	},
+	"PUT /services/todos/{id}" : function() {
+		console.log("Todo updated!");
+		return {};
+	}
 });
 // can.fixture.delay = 1000;
 
-can.fixture("DELETE /services/todos/{id}", function() {
-	return {};
-});
-
 Todo = can.Model.extend({
 	findAll: "GET /services/todos",
-	destroy: "DELETE /services/todos/{id}"
+	destroy: "DELETE /services/todos/{id}",
+	create:  "POST /services/todos",
+	update:  "PUT /services/todos/{id}"
 }, {});
 
 Todo.List = Todo.List.extend({
@@ -39,10 +50,50 @@ Todo.List = Todo.List.extend({
 	}
 });
 
-var todos = new Todo.List({});
-
-var frag = can.view("app-template", {
-	todos: todos
+can.Component.extend({
+	tag: "todos-create",
+	template: '<input id="new-todo" placeholder="What needs to be done?" autofocus="" ' +
+						'can-enter="createTodo" >',
+	scope: {
+		createTodo: function(context, element, event) {
+			if (element.val()) {
+				new Todo({
+					completed: false,
+					name: element.val()
+				}).save();
+				element.val("");
+			}
+		}
+	}
 });
+
+can.Component.extend({
+	tag: "todos-list",
+	template: can.view("todos-list-template"),
+	scope: {
+		editTodo: function(todo, element, event) {
+			todo.attr('editing', true);
+		},
+		updateTodo: function(todo, element, event) {
+			todo.attr('editing', false);
+			todo.attr('name', element.val());
+			todo.save();
+		}
+	}
+});
+
+can.Component.extend({
+	tag: "todos-app",
+	scope: {
+		todos: new Todo.List({})
+	},
+	events: {
+		"{Todo} created": function(Todo, event, newTodo) {
+			this.scope.attr("todos").push(newTodo);
+		}
+	}
+});
+
+var frag = can.view("app-template", {});
 
 $("#app").html( frag );
